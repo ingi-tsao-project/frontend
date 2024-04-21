7f2f7bdbf5120b98d4143b7d58ebc0729e3e32bf
Altraoz <80478887+Altraoz@users.noreply.github.com>
2024-04-21 18:38:52 +0000

    cors config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ingi-tsao frontend
+# sapa
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "Login",
+    methods: {
+        handleSubmit() {
+            console.log("hi");
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        asdfsdfs
+    </h1>
+    <input type="text"  name="textoInput">
+    <input type="password" id="textoInput" name="textoInput">
+
+    <button type="submit">Enviar</button>
+</template>
+
+<style scoped>
+</style>
