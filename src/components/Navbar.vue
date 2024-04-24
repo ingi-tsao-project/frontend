442041b5b8af2b62bd4b7db95fdcc7033e1fae37
Altraoz <curresty@unal.edu.co>
2024-04-24 13:28:22 -0600

    Making overview

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <nav>
+        <div>
+            <h1>
+                All reviews
+            </h1>
+        </div>
+        <div>
+            <img src="../assets/loguito.png" alt="">
+        </div>
+        <div class="div-buttons">
+            <button type="button" class="btn btn btn-outline-light">Login</button>
+            <button type="button" class="btn btn btn-light">Signup</button>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss">
+nav {
+    background-color: rgb(26, 26, 26);
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    color: white;
+    .div-buttons{
+        display: flex;
+        gap: 10px;
+    }
+    h1{
+        font-size:x-large;
+        margin: 0;
+    }
+    img{
+        height: 30px;
+        width: auto;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Navbar from '../components/Navbar.vue'
 </script>
 
 <template>
   <main>
+    <Navbar class="sapa">
+
+    </Navbar>
     <TheWelcome />
   </main>
 </template>
+
+<style lang="scss">
+main{
+  .sapa {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    overflow: hidden;
+    background-color: #333; /* Color de fondo de la barra de navegación */
+    z-index: 999;
+  }
+}
+</style>
